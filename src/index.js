acb7efb3b949c43ea8e5dcecf604b914943577e7
hotgorod <hotgorod@gmail.com>
2023-08-15 17:44:14 +0100

    async await added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-
 const BASE_URL = 'https://pixabay.com/api/'
 const API_KEY = '38827644-52da52720fd8c4c61744e2024'
 
@@ -15,9 +14,6 @@ let currentPage = 1;
 let loadedPhotosCount = 0;
 let requestWord = '';
 
-
-
-
 loadMore.addEventListener('click', onLoad);
 
 function onLoad() {
@@ -27,30 +23,31 @@ function onLoad() {
         .catch(error => { console.log(error) })
 }
 
-
 form.addEventListener('submit', onFormSubmit)
 
-
-
-function onFormSubmit(event){
+async function onFormSubmit(event) {
     event.preventDefault()
     gallery.innerHTML = '';
     loadedPhotosCount = 0;
     currentPage = 1;
     loadMore.hidden = true;
     requestWord = event.target.elements.searchQuery.value;
-        getPhotos(requestWord, currentPage)
-        .then((responce) => {
-            Notify.info(`Hooray! We found ${responce.data.totalHits} images.`);
-            updateScreen(responce)
-        })
-        .catch(error => { console.log(error) })
+
+    try
+    {
+        const responce = await getPhotos(requestWord, currentPage);
+        Notify.info(`Hooray! We found ${responce.data.totalHits} images.`);
+        updateScreen(responce);
     }
+    catch (error) {
+        console.log(error);
+    }
+    
+}
 
 function updateScreen(responce) {
     loadedPhotosCount += responce.data.hits.length;
     
-
     if (loadedPhotosCount < responce.data.totalHits) {
         loadMore.hidden = false;
     } else {
@@ -62,7 +59,6 @@ function updateScreen(responce) {
     const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
     lightbox.refresh();
 }
-
 
 function getPhotos(request, page = 1) {
     return axios.get(`${BASE_URL}?key=${API_KEY}`, {
@@ -78,3 +74,21 @@ function getPhotos(request, page = 1) {
 })
 }
 
+// =======================================
+// left it for myself
+
+// async function onFormSubmit(event) {
+//     event.preventDefault()
+//     gallery.innerHTML = '';
+//     loadedPhotosCount = 0;
+//     currentPage = 1;
+//     loadMore.hidden = true;
+//     requestWord = event.target.elements.searchQuery.value;
+
+//     getPhotos(requestWord, currentPage)
+//     .then((responce) => {
+//         Notify.info(`Hooray! We found ${responce.data.totalHits} images.`);
+//         updateScreen(responce)
+//     })
+//     .catch(error => { console.log(error) })
+// }
